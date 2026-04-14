3cd47d4ef0daac54f02e7fd2b607a27fc3c48886
smurft1aa-lang <smurft1aa-lang@users.noreply.github.com>
2026-04-14 10:43:20 +0800

    fix: add strict_types to footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,7 @@
+<?php declare(strict_types=1); ?>
 </main>
 <script nonce="<?= $GLOBALS['csp_nonce'] ?>" src="/assets/js/main.js"></script>
 </body>
+
 </html>
 <!-- Page rendered in <?= endTimer('page_load') ?>ms -->
